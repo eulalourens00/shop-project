6570ce5a0d3760ca6f3a0efadb73bab3ebf7fc8f
eulalourens00 <kocetovaanastasia19@gmail.com>
2025-01-23 14:26:02 +0300

    main page

--- app/views.py
+++ app/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+
+def hello_world(request):
+    return HttpResponse('Hello World!')
+
+def home(request):
+    return render(request, 'home.html')
 
 # Create your views here.
