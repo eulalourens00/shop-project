e98691013c85a1523c44d6569339859b90f68d08
eulalourens00 <kocetovaanastasia19@gmail.com>
2025-01-30 14:25:09 +0300

    main page

--- app/models.py
+++ app/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+class Product(models.Model):
+    title = models.CharField(max_length=255)
+    description = models.TextField()
+    quantity = models.PositiveIntegerField()
+    price = models.PositiveIntegerField()
+
+    #модель - класс описания предмета
+
+    def __str__(self):
+        return self.title
+
